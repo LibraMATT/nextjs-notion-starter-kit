4e6279cf532174ead7c3188a0787c1b7b9fc2ed2
LibraMATT <98032989+LibraMATT@users.noreply.github.com>
2022-01-19 22:06:18 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '723d283f18c248c89e1536b0e46d1ada',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'SOUR Thinking',
+  domain: 'https://www.notion.so/723d283f18c248c89e1536b0e46d1ada',
+  author: 'Grape Vinegar',
 
   // open graph metadata (optional)
   description: 'Example site description',
